61b55cbfd6fc620301afb377701d7cacc1a3e9ba
has14030 <has14030@spengergasse.at>
2013-12-12 16:43:52 +0100

    finished about page and select game mode page

--- LabPong/LabPong/AboutPage.xaml.cs
+++ LabPong/LabPong/AboutPage.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LabPong
 {
     /// <summary>
-    /// Interaction logic for About.xaml
+    /// Interaction logic for AboutPage.xaml
     /// </summary>
-    public partial class About : Page
+    public partial class AboutPage : Window
     {
-        public About()
+        public AboutPage()
         {
             InitializeComponent();
         }
