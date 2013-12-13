875d5aa792b5d310edec3fd2938f88dbd59cb4f7
bam14053 <abam1993@hotmail.com>
2013-12-13 19:19:18 +0100

    Changed the layout of buttons, created templates

--- LabPong/LabPong/MainPage.xaml.cs
+++ LabPong/LabPong/MainPage.xaml.cs
@@ -22,6 +22,20 @@ namespace LabPong
         public MainPage()
         {
             InitializeComponent();
+            Cursor = Cursors.None;
+        }
+
+        // raised when the mouse pointer moves. 
+        // moves the Ellipse when the mouse moves. 
+        private void Canvas_MouseDown(object sender, MouseEventArgs e){            
+            // Get the x and y coordinates of the mouse pointer.
+            System.Windows.Point position = e.GetPosition(this);
+            double pX = (position.X);
+            double pY = (position.Y);
+
+            // Set the position of the ellipse in the canvas
+            Canvas.SetLeft(pointer, pX);
+            Canvas.SetTop(pointer, pY);
         }
     }
 }
